6b9aa24f2460f6bc7b3932866fee0e17239887dd
andresbueno043 <ajbueno@iegabo.edu.co>
2023-04-09 09:50:25 -0500

    Database models

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,13 @@ import { buttonVariants } from './ui/Button'
 import SignInButton from '@/components/SignInButton'
 import SignOutBotton from '@/components/SignOutButton'
 import ThemeToggle from '@/components/ThemeToggle'
+import { authOptions } from '@/lib/auth'
 
 type Props = {}
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+  const session = await getServerSession(authOptions);
+  
   return (
     <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate700 shadow-sm flex items-center justify-between'>
     <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
